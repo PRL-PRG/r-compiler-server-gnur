27efde02e9240ccbe4089eeffa08f44808ebaa49
Filip Krikava <krikava@gmail.com>
2024-08-21 20:02:18 +0000

    Expose isNull

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -1932,6 +1932,8 @@ SEXP R_bcEncode(SEXP);
 	DECREMENT_REFCNT(dl__x__);		\
     } while (0)
 
+#undef isNull
+#define isNull(s)	(TYPEOF(s) == NILSXP)
 #undef isObject
 #define isObject(s)	(OBJECT(s) != 0)
 
