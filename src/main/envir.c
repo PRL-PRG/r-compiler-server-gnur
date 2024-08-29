97d985fa9c522c27a9f63c52a013d2eb5d59ce65
Filip Krikava <krikava@gmail.com>
2024-08-29 14:01:40 +0000

    Expose R_FindPackageEnv

--- src/main/envir.c
+++ src/main/envir.c
@@ -3726,7 +3726,7 @@ SEXP R_PackageEnvName(SEXP rho)
 	return R_NilValue;
 }
 
-attribute_hidden SEXP R_FindPackageEnv(SEXP info)
+SEXP R_FindPackageEnv(SEXP info)
 {
     SEXP expr, val;
     PROTECT(info);
