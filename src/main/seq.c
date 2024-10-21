d6e14ea1f66a0d5f4385073a822fd3eadc767228
Filip Krikava <krikava@gmail.com>
2024-10-21 05:25:54 +0000

    Expose do_seq*

--- src/main/seq.c
+++ src/main/seq.c
@@ -1053,7 +1053,7 @@ done:
     return ans;
 } // do_seq()
 
-attribute_hidden SEXP do_seq_along(SEXP call, SEXP op, SEXP args, SEXP rho)
+SEXP do_seq_along(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     SEXP ans;
     R_xlen_t len;
@@ -1089,7 +1089,7 @@ attribute_hidden SEXP do_seq_along(SEXP call, SEXP op, SEXP args, SEXP rho)
 	return R_compact_intrange(1, len);
 }
 
-attribute_hidden SEXP do_seq_len(SEXP call, SEXP op, SEXP args, SEXP rho)
+SEXP do_seq_len(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     R_xlen_t len;
 
