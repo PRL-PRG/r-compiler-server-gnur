1fff1ddeec90e48666cd6cde1545a72f42e64721
Filip Krikava <krikava@gmail.com>
2024-12-03 14:18:06 +0000

    Expose asLogical2

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -1974,6 +1974,8 @@ void SET_SCALAR_BVAL(SEXP x, Rbyte v);
 # define PROMISE_TAG(x) 0
 #endif
 
+int Rf_asLogical2(SEXP x, int checking, SEXP call);
+
 // ====================================================================
 // END RSH CHANGES
 // ====================================================================
