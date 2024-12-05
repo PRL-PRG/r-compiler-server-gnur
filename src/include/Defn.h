0781bd49c11219d95214c1f91184689db92cd245
Filip Krikava <krikava@gmail.com>
2024-12-05 17:45:36 +0000

    Expose R_bcstack_t

--- src/include/Defn.h
+++ src/include/Defn.h
@@ -651,37 +651,6 @@ Rboolean (NO_SPECIAL_SYMBOLS)(SEXP b);
 
 #endif /* USE_RINTERNALS */
 
-/* The byte code engine uses a typed stack. The typed stack's entries
-   consist of a tag and a union. An entry can represent a standard
-   SEXP value (tag = 0) or an unboxed scalar value.  For now real,
-   integer, and logical values are supported. It would in principle be
-   possible to support complex scalars and short scalar strings, but
-   it isn't clear if this is worth while.
-
-   In addition to unboxed values the typed stack can hold partially
-   evaluated or incomplete allocated values. For now this is only used
-   for holding a short representation of an integer sequence as produce
-   by the colon operator, seq_len, or seq_along, and as consumed by
-   compiled 'for' loops. This could be used more extensively in the
-   future, though the ALTREP framework may be a better choice.
-
-   Allocating memory on the stack is also supported; this is currently
-   used for jump buffers.
-*/
-typedef struct {
-    int tag;
-    int flags;
-    union {
-	int ival;
-	double dval;
-	SEXP sxpval;
-    } u;
-} R_bcstack_t;
-# define PARTIALSXP_MASK (~255)
-# define IS_PARTIAL_SXP_TAG(x) ((x) & PARTIALSXP_MASK)
-# define RAWMEM_TAG 254
-# define CACHESZ_TAG 253
-
 /* saved bcEval() state for implementing recursion using goto */
 typedef struct R_bcFrame R_bcFrame_type;
 
--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -1969,6 +1969,37 @@ void SET_SCALAR_BVAL(SEXP x, Rbyte v);
 
 int Rf_asLogical2(SEXP x, int checking, SEXP call);
 
+/* The byte code engine uses a typed stack. The typed stack's entries
+   consist of a tag and a union. An entry can represent a standard
+   SEXP value (tag = 0) or an unboxed scalar value.  For now real,
+   integer, and logical values are supported. It would in principle be
+   possible to support complex scalars and short scalar strings, but
+   it isn't clear if this is worth while.
+
+   In addition to unboxed values the typed stack can hold partially
+   evaluated or incomplete allocated values. For now this is only used
+   for holding a short representation of an integer sequence as produce
+   by the colon operator, seq_len, or seq_along, and as consumed by
+   compiled 'for' loops. This could be used more extensively in the
+   future, though the ALTREP framework may be a better choice.
+
+   Allocating memory on the stack is also supported; this is currently
+   used for jump buffers.
+*/
+typedef struct {
+    int tag;
+    int flags;
+    union {
+	int ival;
+	double dval;
+	SEXP sxpval;
+    } u;
+} R_bcstack_t;
+# define PARTIALSXP_MASK (~255)
+# define IS_PARTIAL_SXP_TAG(x) ((x) & PARTIALSXP_MASK)
+# define RAWMEM_TAG 254
+# define CACHESZ_TAG 253
+
 // ====================================================================
 // END RSH CHANGES
 // ====================================================================
