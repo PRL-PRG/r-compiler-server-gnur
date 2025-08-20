fce1c1857ac46ed5a67fde917ffde8645f3190b6
Matej Kocourek <matej@kocourek.cz>
2025-08-20 12:32:58 +0200

    RCP: Improved memory management

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -2099,6 +2099,13 @@ enum {
 // RPC (copy-and-patch)
 // ====================================================================
 
+typedef struct rcp_sharedmem_ptrs
+{
+    void* memory_shared_near;
+    void* memory_shared_low;
+    size_t memory_shared_size;
+} rcp_sharedmem_ptrs;
+
 typedef struct rcp_exec_ptrs
 {
     // Executable code
@@ -2112,12 +2119,9 @@ typedef struct rcp_exec_ptrs
     // Memory management
     void* memory_private;
     size_t memory_private_size;
-    void* memory_shared_near;
-    void* memory_shared_low;
-    size_t memory_shared_size;
-    size_t* memory_shared_refcount;
 } rcp_exec_ptrs;
 
+void R_RcpSharedFree(SEXP);
 void R_RcpFree(SEXP);
 
 // ====================================================================
