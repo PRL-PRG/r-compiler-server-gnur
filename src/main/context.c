67f4e2d0efb3a15b706069f05104b2c792067dda
Filip Krikava <krikava@gmail.com>
2025-09-25 18:43:56 +0000

    Expose findcontext

--- src/main/context.c
+++ src/main/context.c
@@ -331,7 +331,7 @@ void endcontext(RCNTXT * cptr)
 
 /* findcontext - find the correct context */
 
-attribute_hidden void NORET findcontext(int mask, SEXP env, SEXP val)
+void NORET findcontext(int mask, SEXP env, SEXP val)
 {
     RCNTXT *cptr;
     cptr = R_GlobalContext;
