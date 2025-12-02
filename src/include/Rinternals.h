66ef28767aa38511a4503f0883ddd5967574fd38
Matej Kocourek <matej@kocourek.cz>
2025-12-02 16:10:04 +0100

    Support not-inlined functions in RCP

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -2104,6 +2104,8 @@ typedef struct rcp_sharedmem_ptrs
     void* memory_shared_near;
     void* memory_shared_low;
     size_t memory_shared_size;
+    void* memory_functions_executable;
+    size_t memory_functions_executable_size;
 } rcp_sharedmem_ptrs;
 
 typedef struct rcpEval_locals {
