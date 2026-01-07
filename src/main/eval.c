08a56bb2cc67baad0c14bcd440b68e4830453c43
Filip Krikava <krikava@gmail.com>
2026-01-07 16:26:07 +0000

    Add Rsh_is_closure

--- src/main/eval.c
+++ src/main/eval.c
@@ -1842,6 +1842,11 @@ static R_INLINE Rboolean jit_srcref_match(SEXP cmpsrcref, SEXP srcref)
 {
     return R_compute_identical(cmpsrcref, srcref, 0);
 }
+ 
+Rboolean Rsh_is_closure(SEXP clo) {
+  return TYPEOF(clo) == CLOSXP && TYPEOF(BODY(clo)) == EXTPTRSXP && RSH_IS_CLOSURE_BODY(BODY(clo));
+}
+
 
 attribute_hidden SEXP R_cmpfun1(SEXP fun)
 {
@@ -1854,7 +1859,7 @@ attribute_hidden SEXP R_cmpfun1(SEXP fun)
     PROTECT(fcall = lang3(R_TripleColonSymbol, packsym, funsym));
     PROTECT(call = lang2(fcall, fun));
     PROTECT(val = eval(call, R_GlobalEnv));
-    if (TYPEOF(BODY(val)) != BCODESXP)
+    if (!Rsh_is_closure(val) && TYPEOF(BODY(val)) != BCODESXP)
 	/* Compilation may have failed because R allocator could not malloc
 	   memory to extend the R heap, so we run GC to release some pages.
 	   This problem has been observed while byte-compiling packages on
@@ -1920,7 +1925,7 @@ static void R_cmpfun(SEXP fun)
 
     SEXP val = R_cmpfun1(fun);
 
-    if (TYPEOF(BODY(val)) != BCODESXP)
+    if (!Rsh_is_closure(val) && TYPEOF(BODY(val)) != BCODESXP)
 	SET_NOJIT(fun);
     else {
 	if (jit_strategy != STRATEGY_NO_CACHE)
