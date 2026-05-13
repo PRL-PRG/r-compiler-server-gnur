9455f9dc7772d95d38a004212c3b20f7911d1686
Matej Kocourek <matej@kocourek.cz>
2026-05-13 22:40:15 +0200

    Expose some functions

--- src/main/envir.c
+++ src/main/envir.c
@@ -875,7 +875,7 @@ attribute_hidden void unbindVar(SEXP symbol, SEXP rho)
   Callers set *canCache = TRUE or NULL
 */
 
-static SEXP findVarLocInFrame(SEXP rho, SEXP symbol, Rboolean *canCache)
+SEXP findVarLocInFrame(SEXP rho, SEXP symbol, Rboolean *canCache)
 {
     int hashcode;
     SEXP frame, c;
@@ -1254,7 +1254,7 @@ SEXP findVar(SEXP symbol, SEXP rho)
     return R_findVar(symbol, rho);
 }
 
-static SEXP findVarLoc(SEXP symbol, SEXP rho)
+SEXP findVarLoc(SEXP symbol, SEXP rho)
 {
     SEXP vl;
 
@@ -4628,4 +4628,3 @@ attribute_hidden void findFunctionForBody(SEXP body) {
 	}
     }
 }
-
