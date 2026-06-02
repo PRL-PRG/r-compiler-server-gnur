662ac28aed9a89eadc1b2b97052041999193a415
Matej Kocourek <matej@kocourek.cz>
2026-06-02 19:56:57 +0200

    Fix RCP closures

--- src/main/eval.c
+++ src/main/eval.c
@@ -5571,9 +5571,7 @@ static SEXP bytecodeExpr(SEXP e)
     }
 	else if(RSH_IS_JIT_PTR(e)) {
 	SEXP consts = RSH_JIT_CONSTS(e);
-	if (LENGTH(consts) > 0)
 	    return VECTOR_ELT(consts, 0);
-	else return R_NilValue;
 	}
     else return e;
 }
